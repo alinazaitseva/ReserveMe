79b7e60b0cc029b98de4813e521301be94138bb5
zaitseva.alina <1823345@gmail.com>
2018-02-27 13:59:44 +0200

    add image background

--- reserve/src/components/App.js
+++ reserve/src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 class App extends Component {
   render() {
     return (
-        <div className="container">
-          <div></div>
+        <div className="container Hungry-background">
+          <div>kjhgfghjhgfd</div>
         </div>
 
     );
