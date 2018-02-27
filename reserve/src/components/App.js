5b344ae43d6f48305d8dc0bf77bd13d8abc78464
zaitseva.alina <1823345@gmail.com>
2018-02-27 15:43:26 +0200

    layout

--- reserve/src/components/App.js
+++ reserve/src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import { Grid, Row, Col } from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
     return (
-        <div className="container Hungry-background">
-          <div>kjhgfghjhgfd</div>
+        <div className="container-fluid Hungry-background">
+          <Row>
+            <Col lg={12} md={12} xs={12}>
+              <div className="rectangle">
+                
+              </div>
+            </Col>
+          </Row>
         </div>
-
     );
   }
 }
